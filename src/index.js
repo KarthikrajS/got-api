065ef333204a4957ae130bdb1013666df5724f76
moovendan mahalakshmi <karthikraj1995@gmail.com>
2020-05-24 21:04:04 +0530

    path.resolve()

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL,{ useUnifiedTopology: true , useNewUrlParser: true})
 app.use('/api/battleData',battleData);
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
+    res.sendFile(path.join(__dirname,'src/index.html'));
 })
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,()=> console.log(`running on ${PORT}`));
