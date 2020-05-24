2351a7763c2161b95c272d0cb59b251f793ba64e
moovendan mahalakshmi <karthikraj1995@gmail.com>
2020-05-24 18:43:05 +0530

    redo

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import battleData from './routes/battleData';
+import dotenv from 'dotenv';
+import Promise from 'bluebird';
+
+dotenv.config()
+const app = express();
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL,{ useUnifiedTopology: true , useNewUrlParser: true})
+app.use('/api/battleData',battleData);
+app.get('/*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=> console.log(`running on ${PORT}`));
