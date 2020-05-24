7a10ef01a2ec601d15630bcd9311bc0685c05fa6
moovendan mahalakshmi <karthikraj1995@gmail.com>
2020-05-24 17:33:13 +0530

    index.html

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ import index from './index.html'
 
 dotenv.config()
 const app = express();
+
+const __dirname = path.resolve()
 app.use(bodyParser.json());
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL,{ useUnifiedTopology: true , useNewUrlParser: true})
 app.use('/api/battleData',battleData);
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname,index));
+    res.sendFile(path.join(__dirname,'index.html'));
 })
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,()=> console.log(`running on ${PORT}`));
