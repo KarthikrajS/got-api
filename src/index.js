94f238dd37f4e13190b8d501445c339b6cc83b2d
KarthikrajS <karthikraj1995@gmail.com>
2020-05-26 19:27:33 +0530

    cors test

--- src/index.js
+++ src/index.js
@@ -10,25 +10,14 @@ import cors from 'cors'
 dotenv.config()
 const __dirname = path.resolve()
 const app = express();
-var whitelist =['https://battleapp-got.herokuapp.com/']
 
 app.use(bodyParser.json());
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL,{ useUnifiedTopology: true , useNewUrlParser: true})
+
 app.use('/api/battleData',battleData);
-var corsOption ={
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1){
-            callback(null, true)
-        }
-        else{
-            callback(new Error('Not allowed by cors'))
-        }
-
-    }
-}
-
-app.get('/*', cors(corsOption),(req,res)=>{
+app.use(cors())
+app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,'src/index.html'));
 })
 const PORT = process.env.PORT || 8080;
