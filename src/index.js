0b66e489e5b1360bc6a369cd836a56a22f3d2316
KarthikrajS <karthikraj1995@gmail.com>
2020-05-26 16:53:50 +0530

    change

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import bodyParser from 'body-parser';
 import battleData from './routes/battleData';
 import dotenv from 'dotenv';
 import Promise from 'bluebird';
+import cors from 'cors'
 
 dotenv.config()
 const __dirname = path.resolve()
 const app = express();
+app.use(cors())
 app.use(bodyParser.json());
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL,{ useUnifiedTopology: true , useNewUrlParser: true})
@@ -17,4 +19,5 @@ app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,'src/index.html'));
 })
 const PORT = process.env.PORT || 8080;
-app.listen(PORT,'0.0.0.0',()=> console.log(`running on ${PORT}`));
+
+app.listen(PORT,()=> console.log(`running on ${PORT}`));
